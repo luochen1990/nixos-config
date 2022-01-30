1892bde673bdcdac868e4e06132bacd256cd42ee
LuoChen <luochen1990@gmail.com>
2022-01-30 16:20:17 +0800

    release: 2022-01-30 -- 06dfbb9

--- flake.nix
+++ flake.nix
@@ -10,44 +10,34 @@
   outputs = { self, nixpkgs }: { #, rust-overlay }: {
 
     nixosConfigurations = let
-        dirContents = builtins.readDir ./profiles;
-        profileNames = builtins.filter (name: dirContents.${name} == "directory") (builtins.attrNames dirContents);
+      dirContents = builtins.readDir ./profiles;
+      profileNames = builtins.filter (name: dirContents.${name} == "directory") (builtins.attrNames dirContents);
 
-        profileEntry = profileName: let
-          profilePath = (./profiles + "/${profileName}");
-          config = { inherit profileName; };
+    in builtins.listToAttrs (builtins.map (profileName: {
+      name = profileName;
+      value =  nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          { inherit profileName; }
+          (./profiles + "/${profileName}/configuration.nix")
 
-          defaultProfile = nixpkgs.lib.nixosSystem rec {
-            system = "x86_64-linux";
-            modules = [
-              #({ pkgs, ... }: {
-              #  nixpkgs.overlays = [ rust-overlay.overlay ];
-              #  environment.systemPackages = [ ( pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; } ) ];
-              #})
-              config
-              (./profiles + "/${profileName}/configuration.nix")
-              #home-manager.nixosModules.home-manager
-              #{
-              #  home-manager.useGlobalPkgs = true;
-              #  home-manager.useUserPackages = true;
-              #  home-manager.users.lc = import ./home.nix;
-              #}
-            ];
-          };
-        in {
-          name = profileName;
-          value = if builtins.pathExists (profilePath + "/default.nix")
-            then import profilePath nixpkgs config
-            else defaultProfile;
-        };
+          #({ pkgs, ... }: {
+          #  nixpkgs.overlays = [ rust-overlay.overlay ];
+          #  environment.systemPackages = [ ( pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; } ) ];
+          #})
 
-        profiles = builtins.listToAttrs (builtins.map profileEntry profileNames);
+          #home-manager.nixosModules.home-manager
+          #{
+          #  home-manager.useGlobalPkgs = true;
+          #  home-manager.useUserPackages = true;
+          #  home-manager.users.lc = import ./home.nix;
+          #}
+        ];
+      };
+    }) profileNames);
 
-    in profiles // {
-      # use command `hostname` to check your current host name
-      # NOTE: for adhoc use case, please use `nixos-rebuild switch --flake /path/of/dir/which/contains/flake-dot-nix/#{profileName}` instead.
-      #"lc-nixos" = profiles.pocket-nixos;
-      #"nixos" = profiles.wsl-nixos; #be careful about enable this alias, since nixos is the default host name for system which installed from iso
-    };
+    # NOTE: 
+    #  The command `nixos-rebuild switch --flake .` choose profile via `hostname`
+    #  If you want to specify profile, use `nixos-rebuild switch --flake .#{profileName}` instead.
   };
 }
