48f1ef2bb6961be62e62d4bcdcd3c0ced42fe62d
LuoChen <luochen1990@gmail.com>
2022-08-23 15:35:28 +0800

    improve: modularity of flake.nix

--- flake.nix
+++ flake.nix
@@ -1,43 +1,49 @@
+/*
+Usage: 
+
+`nixos-rebuild switch --flake .`
+
+The above command auto decide which profile to use via `hostname`.
+
+If you want to specify profile name manually, use following command instead:
+
+`nixos-rebuild switch --flake .#<profileName>`
+*/
+
 {
   description = "My NixOS";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    #rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs"; };
+    flake-utils.url = "github:numtide/flake-utils";
+    nixos-cn.url = "github:nixos-cn/flakes";
+    nixos-cn.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-cn.inputs.flake-utils.follows = "flake-utils";
     #home-manager = {url = "github:nix-community/home-manager"; inputs.nixpkgs.follows = "nixpkgs"; };
+    #rust-overlay = { url = "github:oxalica/rust-overlay"; inputs.nixpkgs.follows = "nixpkgs"; };
+    #resign = { url = "github:NickCao/resign"; inputs.nixpkgs.follows = "nixpkgs"; };
   };
 
-  outputs = { self, nixpkgs }: { #, rust-overlay }: {
-
-    nixosConfigurations = let
-      dirContents = builtins.readDir ./profiles;
-      profileNames = builtins.filter (name: dirContents.${name} == "directory") (builtins.attrNames dirContents);
-
-    in builtins.listToAttrs (builtins.map (profileName: {
-      name = profileName;
-      value =  nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          { inherit profileName; }
-          (./profiles + "/${profileName}/configuration.nix")
-
-          #({ pkgs, ... }: {
-          #  nixpkgs.overlays = [ rust-overlay.overlay ];
-          #  environment.systemPackages = [ ( pkgs.rust-bin.stable.latest.default.override { extensions = [ "rust-src" ]; } ) ];
-          #})
-
-          #home-manager.nixosModules.home-manager
-          #{
-          #  home-manager.useGlobalPkgs = true;
-          #  home-manager.useUserPackages = true;
-          #  home-manager.users.lc = import ./home.nix;
-          #}
-        ];
-      };
-    }) profileNames);
-
-    # NOTE: 
-    #  The command `nixos-rebuild switch --flake .` choose profile via `hostname`
-    #  If you want to specify profile, use `nixos-rebuild switch --flake .#{profileName}` instead.
-  };
+  outputs = { self, nixpkgs, flake-utils, ... }@extra-args: 
+    #flake-utils.lib.eachDefaultSystem (system:
+    let system = "x86_64-linux"; in
+    let pkgs = nixpkgs.legacyPackages.${system}; in
+    let lib = nixpkgs.lib; in
+    let subDirs = (path: with builtins; let d = readDir path; in filter (k: d.${k} == "directory") (attrNames d)); in
+    {
+      nixosConfigurations = lib.genAttrs (subDirs ./profiles) (profileName:
+        lib.nixosSystem rec {
+          inherit system;
+          specialArgs = extra-args // {inherit system;};
+          modules = [
+            {
+              inherit profileName; 
+              nix.settings.nix-path = [ "nixpkgs=${nixpkgs}" ];
+            }
+            (./profiles + "/${profileName}/configuration.nix")
+          ];
+        }
+      );
+    };
+    #});
 }
